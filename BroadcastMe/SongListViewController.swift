f758fd663073383c0c68d21ac4e942be247bc4a2
timtime4 <tpusateri44@gmail.com>
2015-06-25 22:18:16 -0400

    First Commit
    Signed-off-by: timtime4 <tpusateri44@gmail.com>

---
+++ BroadcastMe/SongListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController2ViewController.swift
+//  BroadcastMe
+//
+//  Created by Tim Pusateri on 6/25/15.
+//  Copyright (c) 2015 Tim Pusateri. All rights reserved.
+//
+
+import UIKit
+
+class SongListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
+    
+
+}
--- BroadcastMe/ViewController.swift
+++ BroadcastMe/ViewController.swift
@@ -8,17 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
+    @IBAction func soundsGoodButtonPressed(sender: AnyObject) {
+        println("Sounds Good button pressed, transitioning to table")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("Hello, we've started up the app")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+        
+        println("preparing for segue\n")
+    }
+    
+
 
 
 }
